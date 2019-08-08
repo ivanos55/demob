c38a054cab3d8ea80ba60bff5bd3bfefb104f2dd
Ivan Acevedo <ivan@hde.mx>
2019-08-08 17:33:24 -0500

    Se agrega funcionalidad de suma y resta

---
+++ calc.py
@@ -0,0 +1,6 @@
+def suma(valor1, valor2):
+    return valor1+valor2
+
+def resta(valor1, valor2):
+    return valor1-valor2
+
--- hola.py
+++ hola.py
@@ -1,1 +1,10 @@
+import calc
+
 print('Hola!')
+
+
+print('Suma 2+3:')
+print(calc.suma(2,3))
+
+print('Suma 2-3:')
+print(calc.resta(2,3))
