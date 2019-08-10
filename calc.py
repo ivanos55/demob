49d4dab77af4852c13a7e73523dbf4e04db6feb0
Ivan Acevedo <ivan@hde.mx>
2019-08-10 09:27:36 -0500

    Se agrega multiplica y divide

--- calc.py
+++ calc.py
@@ -4,3 +4,11 @@ def suma(valor1, valor2):
 def resta(valor1, valor2):
     return valor1-valor2
 
+def multiplica(valor1, valor2):
+    return valor1*valor2
+
+def divide(valor1, valor2):
+    if valor2 is 0:
+        return "No dividir entre cero"
+    return valor1/valor2
+
--- hola.py
+++ hola.py
@@ -9,4 +9,14 @@ print(calc.suma(2,3))
 
 print('Suma 2-3:')
 print(calc.resta(2,3))
+
+
 print(trigonometria.senoDelAngulo(0))
+
+
+print('Multi 2*3:')
+print(calc.multiplica(2,3))
+
+print('Divide 2/3:')
+print(calc.divide(2,3))
+
